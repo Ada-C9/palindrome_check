3335d3010bd366337e940098dd99ac4d6c88ade6
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-03-22 17:16:17 -0700

    All tests passing with front and back variables

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,28 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+
+  return false if my_phrase.nil?
+
+  front = 0
+  back = my_phrase.length - 1
+
+  until front >= back
+
+    while my_phrase[front] == " "
+      front += 1
+    end
+
+    while my_phrase[back] == " "
+      back -= 1
+    end
+
+    return false if my_phrase[front] != my_phrase[back]
+
+    front += 1
+    back -= 1
+  end
+
+  return true
+
 end
