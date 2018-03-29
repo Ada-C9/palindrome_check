36cbd802c0488b68899cb15001446eaaa07cbec3
Nora Peters <norapeters5@gmail.com>
2018-03-29 11:51:55 -0700

    all tests passing

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -4,13 +4,17 @@ def palindrome_check(my_phrase)
   return false if my_phrase.nil?
   i = 0
   j = my_phrase.length - 1
-  while i <= my_phrase.length
-    if my_phrase[i] == my_phrase[j]
+  while i < my_phrase.length / 2
+    if my_phrase[i] == " "
+      i += 1
+    elsif my_phrase[j] == " "
+      j -= 1
+    elsif my_phrase[i] != my_phrase[j]
+      return false
+    else
       i += 1
       j -= 1
-    else
-      return false
     end
-    return true
   end
+  return true
 end
