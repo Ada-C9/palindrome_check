7f664ce7fae7f751fdbe869ccf1df9b38b964c91
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-29 21:07:17 -0700

    another solution for palindrome thought of through integer palindrome method to reduce space complexity

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,31 +1,20 @@
-require 'pry'
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
   return false if my_phrase == nil
-  reversed_phrase = string_reverse(my_phrase)
-  is_palindrome(my_phrase, reversed_phrase)
-end
-
-def is_palindrome(some_phrase, reversed_phrase)
-  i = 0
-  some_phrase.length.times do |x|
-    if some_phrase[x] != " " && some_phrase[x] != reversed_phrase[i]
+  j = 0
+  k = my_phrase.length - 1
+  while j < k
+    if my_phrase[j] == " "
+      j += 1
+    elsif my_phrase[k] == " "
+      k -= 1
+    elsif my_phrase[j] == my_phrase[k]
+      j += 1
+      k -= 1
+    elsif my_phrase[k] != " " && my_phrase[k] != " " && my_phrase[j] != my_phrase[k]
       return false
-    elsif some_phrase[x] == reversed_phrase[i]
-      i += 1
     end
   end
-  return true
-end
-
-def string_reverse(my_string)
-  return my_string if my_string == nil
-  k = my_string.length - 1
-  reversed_string = ""
-  while k >= 0
-    reversed_string << my_string[k] if my_string[k] != " "
-    k -= 1
-  end
-  reversed_string
+  true
 end
