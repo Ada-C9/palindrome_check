ec51fc61550b6cb421c2763582b3045e048c3ced
Abiaina <abinnet.a@gmail.com>
2018-04-02 22:38:57 -0700

    detects pallindromes with spaces

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,19 +1,39 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
+require 'pry'
 def palindrome_check(my_phrase)
   if my_phrase == nil
     return false
   end
 
-  x = 0
+  foward_index = 0
+  backward_index = (my_phrase.length - 1)
+
   palindrome = true
 
-  while x < my_phrase.length
-    if my_phrase[x] != my_phrase[((my_phrase.length - 1) - x)]
+  while foward_index < (my_phrase.length - 1)
+    if my_phrase[foward_index] == " "
+      while my_phrase[foward_index] == " "
+        foward_index += 1
+      end
+    end
+
+    if my_phrase[backward_index] == " "
+      while my_phrase[backward_index] == " "
+        backward_index -= 1
+      end
+    end
+
+    puts " #{my_phrase[foward_index]} : #{my_phrase[backward_index]}"
+
+    if my_phrase[foward_index] != my_phrase[backward_index]
       palindrome = false
     end
-    x += 1
-  end
 
+    foward_index += 1
+    backward_index -= 1
+  end
   return palindrome
 end
+
+puts palindrome_check(" pull   up ")
