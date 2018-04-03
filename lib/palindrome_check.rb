a81cda505a6ab67e9037320d0b6e0ef125d1926d
Alexandria Brown <axbn2190@gmail.com>
2018-04-02 21:51:07 -0700

    accounts for edge cases with spaces

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,25 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  if my_phrase == nil
+    return false
+  elsif my_phrase.length <= 1
+    return true
+  end
+
+  first = 0
+  last = my_phrase.length - 1
+
+  while first < last
+    if my_phrase[first] == " " || my_phrase[last] == " "
+      first += 1
+      last -= 1
+    elsif my_phrase[first] == my_phrase[last]
+      first += 1
+      last -= 1
+      return true
+    else
+      return false
+    end
+  end
 end
