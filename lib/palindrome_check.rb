810f3954e38dadf401eed1b3b4ef922c774ae8a7
Abiaina <abinnet.a@gmail.com>
2018-04-02 21:44:14 -0700

    detects palindromes without spaces

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,19 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  if my_phrase == nil
+    return false
+  end
+
+  x = 0
+  palindrome = true
+
+  while x < my_phrase.length
+    if my_phrase[x] != my_phrase[((my_phrase.length - 1) - x)]
+      palindrome = false
+    end
+    x += 1
+  end
+
+  return palindrome
 end
