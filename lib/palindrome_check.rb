05bd7a6ec7c07f932825a4380b57ecb38d7c9f2f
Maja Graonic <m.graonic@gmail.com>
2018-04-07 21:25:42 -0700

    all tests passing

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,23 +1,28 @@
+require 'pry'
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  if my_phrase == nil
-    return false
-  end
-
-  if my_phrase.length == 1 || 0
-    return true
-  end
+  return false if my_phrase == nil
+  return true if my_phrase.length == 1 || my_phrase.length == 0
 
   left = 0
   right = my_phrase.length - 1
 
   while left < right
-    if my_phrase[left]!= my_phrase[right - left]
+    while my_phrase[left] == " "
+      left += 1
+    end
+
+    while my_phrase[right] == " "
+      right -= 1
+    end
+
+    if my_phrase[left]!= my_phrase[right]
       return false
     else
       left += 1
-      return true
+      right -= 1
     end
   end
+  return true
 end
